72c8285c0c0b8d2ec12eab83efbb88b32afa9fd6
GeorgePanagiotakis14 <panagiotakisg4@gmail.com>
2026-01-07 09:18:03 +0200

    Delete excel_form_app directory

--- excel_form_app/excel_form_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # make sure to import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # include your app URLs
-]
